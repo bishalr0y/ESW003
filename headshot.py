b89f1103b1a0b749f6c06a16ebfbe6fc49b62c60
Bishal Roy <bishalroy895@gmail.com>
2023-07-16 11:54:07 +0530

    add directory to store the 5 pics of the user

--- headshot.py
+++ headshot.py
@@ -1,30 +1,31 @@
 import cv2
 import sys
+import os
 
-directory_name = "faces"
+def click_pics(directory_name):
+    faces_folder = "faces"
+    if not os.path.exists(faces_folder):
+        os.makedirs(faces_folder)
 
-name = sys.argv[1] # for taking command line argument
+    directory_path = os.path.join(faces_folder, directory_name)
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
 
-def click_pic(name):
-    key = cv2. waitKey(1)
+    key = cv2.waitKey(1)
     webcam = cv2.VideoCapture(0)
-    while True:
+    count = 1
+
+    while count <= 5:
         try:
             check, frame = webcam.read()
-            print(check) #prints true as long as the webcam is running
-            # print(frame) #prints matrix values of each framecd 
+            print(check)  # prints true as long as the webcam is running
             cv2.imshow("Capturing", frame)
             key = cv2.waitKey(1)
-            if key == ord('s'): 
-                cv2.imwrite(filename=directory_name + "/" + name+'.jpg', img=frame)
-                webcam.release()
-                img_new = cv2.imread(directory_name + "/" + name+'.jpg', cv2.IMREAD_GRAYSCALE)
-                img_new = cv2.imshow("Captured Image", img_new)
-                cv2.waitKey(1650)
-                cv2.destroyAllWindows()
-                print("Processing image...")
-                print("Image saved!")
-                break
+            if key == ord('s'):
+                file_name = os.path.join(directory_path, str(count) + '.jpg')
+                cv2.imwrite(filename=file_name, img=frame)
+                print("Image " + str(count) + " saved!")
+                count += 1
 
             elif key == ord('q'):
                 print("Turning off camera.")
@@ -33,14 +34,77 @@ def click_pic(name):
                 print("Program ended.")
                 cv2.destroyAllWindows()
                 break
-            
-        except(KeyboardInterrupt):
+
+        except KeyboardInterrupt:
             print("Turning off camera.")
             webcam.release()
             print("Camera off.")
             print("Program ended.")
             cv2.destroyAllWindows()
+            break
+
+    webcam.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    click_pic(name)    
+    if len(sys.argv) < 2:
+        print("Please provide the directory name as a command line argument.")
+    else:
+        directory_name = sys.argv[1]
+        click_pics(directory_name)
+import cv2
+import sys
+import os
+
+def click_pics(directory_name):
+    faces_folder = "faces"
+    if not os.path.exists(faces_folder):
+        os.makedirs(faces_folder)
+
+    directory_path = os.path.join(faces_folder, directory_name)
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    key = cv2.waitKey(1)
+    webcam = cv2.VideoCapture(0)
+    count = 1
+
+    while count <= 5:
+        try:
+            check, frame = webcam.read()
+            print(check)  # prints true as long as the webcam is running
+            cv2.imshow("Capturing", frame)
+            key = cv2.waitKey(1)
+            if key == ord('s'):
+                file_name = os.path.join(directory_path, str(count) + '.jpg')
+                cv2.imwrite(filename=file_name, img=frame)
+                print("Image " + str(count) + " saved!")
+                count += 1
+
+            elif key == ord('q'):
+                print("Turning off camera.")
+                webcam.release()
+                print("Camera off.")
+                print("Program ended.")
+                cv2.destroyAllWindows()
+                break
+
+        except KeyboardInterrupt:
+            print("Turning off camera.")
+            webcam.release()
+            print("Camera off.")
+            print("Program ended.")
+            cv2.destroyAllWindows()
+            break
+
+    webcam.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide the directory name as a command line argument.")
+    else:
+        directory_name = sys.argv[1]
+        click_pics(directory_name)
